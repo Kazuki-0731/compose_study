e37b548fd69de798642b137ceaeb10425e0dda48
Kazuki-0731 <meganekko.gekiloving@gmail.com>
2021-05-23 18:11:19 +0900

    CameraCaptureにカメラ処理を移行

--- app/src/main/java/com/example/mapping_sample/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mapping_sample/ui/dashboard/DashboardFragment.kt
@@ -29,20 +29,15 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mapping_sample.databinding.FragmentDashboardBinding
 import java.util.*
 
-@Suppress("DEPRECATION")
 class DashboardFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
 
-    private var captureSession: CameraCaptureSession? = null
-    private var cameraDevice: CameraDevice? = null
-    private val previewSize: Size = Size(300, 300) // FIXME: for now.
-    private lateinit var previewRequestBuilder: CaptureRequest.Builder
-    private var imageReader: ImageReader? = null
-    private lateinit var previewRequest: CaptureRequest
-    private var backgroundThread: HandlerThread? = null
-    private var backgroundHandler: Handler? = null
+    var backgroundThread: HandlerThread? = null
+    var backgroundHandler: Handler? = null
+
+    private val cameraCapture:CameraCapture = CameraCapture()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,13 +48,10 @@ class DashboardFragment : Fragment() {
             ViewModelProvider(this).get(DashboardViewModel::class.java)
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-//        val textView: TextView = binding.textDashboard
-//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
 
-        binding.cameraPreview.surfaceTextureListener = surfaceTextureListener
-        startBackgroundThread()
+        binding.buttonCapture.setOnClickListener {
+            // カメラでキャプション
+        }
 
         return root
     }
@@ -72,21 +64,9 @@ class DashboardFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        binding.buttonCapture.setOnClickListener {
-            // カメラ機能を実装したアプリが存在するかチェック
-            if (checkCameraPermission()) {
-                takePicture()
-            } else {
-                grantCameraPermission()
-            }
-//            Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(requireActivity().packageManager)?.let {
-//                if (checkCameraPermission()) {
-//                    takePicture()
-//                } else {
-//                    grantCameraPermission()
-//                }
-//            } ?: Toast.makeText(requireContext(), "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
-        }
+        cameraCapture.initCameraCapture(binding.cameraPreview, requireActivity(), requireContext(), this)
+        cameraCapture.textureView.surfaceTextureListener = cameraCapture.surfaceTextureListener
+        startBackgroundThread()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -100,137 +80,38 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<out String>,
-                                            grantResults: IntArray) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                takePicture()
-            }
-        }
-    }
-
-    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-            imageReader = ImageReader.newInstance(width, height,
-                ImageFormat.JPEG, /*maxImages*/ 2)
-            openCamera()
-        }
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, p1: Int, p2: Int) {}
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
-            return false
-        }
-    }
-
-    fun openCamera() {
-        var manager: CameraManager = requireActivity().getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
-        try {
-            var camerId: String = manager.getCameraIdList()[0]
-            val permission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
-
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                requestCameraPermission()
-                return
-            }
-            manager.openCamera(camerId, stateCallback, null)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private val stateCallback = object : CameraDevice.StateCallback() {
-        override fun onOpened(cameraDevice: CameraDevice) {
-            this@DashboardFragment.cameraDevice = cameraDevice
-            createCameraPreviewSession()
-        }
-        override fun onDisconnected(cameraDevice: CameraDevice) {
-            cameraDevice.close()
-            this@DashboardFragment.cameraDevice = null
-        }
-        override fun onError(cameraDevice: CameraDevice, error: Int) {
-            onDisconnected(cameraDevice)
-            requireActivity().finish()
-        }
-    }
-
-    fun requestCameraPermission() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-            AlertDialog.Builder(requireContext())
-                .setMessage("Permission Here")
-                .setPositiveButton(android.R.string.ok) { _, _ ->
-                    requestPermissions(arrayOf(Manifest.permission.CAMERA),
-                        200)
-                }
-                .setNegativeButton(android.R.string.cancel) { _, _ ->
-                    requireActivity().finish()
-                }
-                .create()
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), 200)
-        }
-    }
-
-    private fun createCameraPreviewSession() {
-        try {
-            val texture = binding.cameraPreview.surfaceTexture
-            texture!!.setDefaultBufferSize(previewSize.width, previewSize.height)
-            val surface = Surface(texture)
-            previewRequestBuilder = cameraDevice!!.createCaptureRequest(
-                CameraDevice.TEMPLATE_PREVIEW
-            )
-            previewRequestBuilder.addTarget(surface)
-            cameraDevice?.createCaptureSession(listOf(surface, imageReader?.surface),
-                @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-                object : CameraCaptureSession.StateCallback() {
-
-                    override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
-
-                        if (cameraDevice == null) return
-                        captureSession = cameraCaptureSession
-                        try {
-                            previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
-                            previewRequest = previewRequestBuilder.build()
-                            captureSession?.setRepeatingRequest(previewRequest,
-                                null, Handler(backgroundThread?.looper!!))
-                        } catch (e: CameraAccessException) {
-                            Log.e("erfs", e.toString())
-                        }
-                    }
-
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                        Toast.makeText(requireContext(), "Failed!!", Toast.LENGTH_SHORT).show()
-                    }
-                }, null)
-        } catch (e: CameraAccessException) {
-            Log.e("erf", e.toString())
-        }
-    }
+//    override fun onRequestPermissionsResult(requestCode: Int,
+//                                            permissions: Array<out String>,
+//                                            grantResults: IntArray) {
+//        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+//            if (grantResults.isNotEmpty() &&
+//                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                takePicture()
+//            }
+//        }
+//    }
 
     private fun startBackgroundThread() {
         backgroundThread = HandlerThread("CameraBackground").also { it.start() }
         backgroundHandler = Handler(backgroundThread?.looper!!)
     }
 
-    private fun takePicture() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
-            addCategory(Intent.CATEGORY_DEFAULT)
-        }
-
-        startActivityForResult(intent, CAMERA_REQUEST_CODE)
-    }
-
-    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED ==
-            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
-
-    private fun grantCameraPermission() =
-        ActivityCompat.requestPermissions(requireActivity(),
-            arrayOf(Manifest.permission.CAMERA),
-            CAMERA_PERMISSION_REQUEST_CODE)
-
+//    private fun takePicture() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+//            addCategory(Intent.CATEGORY_DEFAULT)
+//        }
+//
+//        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+//    }
+//
+//    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED ==
+//            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+//
+//    private fun grantCameraPermission() =
+//        ActivityCompat.requestPermissions(requireActivity(),
+//            arrayOf(Manifest.permission.CAMERA),
+//            CAMERA_PERMISSION_REQUEST_CODE)
+//
     companion object {
         const val CAMERA_REQUEST_CODE = 1
         const val CAMERA_PERMISSION_REQUEST_CODE = 2
