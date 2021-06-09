9ad8635ad92893e7a34ffa1da8b6b5c9abae9519
Ishino Kazuki <ishinokazuki@team-lab.com>
2021-06-09 14:42:58 +0900

    gradle update / remove useless

--- app/src/main/java/com/example/mapping_sample/ui/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mapping_sample/ui/ui/home/HomeFragment.kt
@@ -4,22 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.InternalComposeUiApi
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.WindowRecomposerFactory
 import androidx.compose.ui.platform.compositionContext
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewTreeLifecycleOwner
-import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.example.mapping_sample.R
 import com.example.mapping_sample.databinding.FragmentHomeBinding
 import java.util.concurrent.atomic.AtomicReference
@@ -51,9 +43,6 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root.apply {
-            ViewTreeLifecycleOwner.set(this, viewLifecycleOwner)
-            val newRecomposer = AtomicReference(WindowRecomposerFactory.LifecycleAware).get().createRecomposer(rootView)
-            compositionContext = newRecomposer
             findViewById<ComposeView>(R.id.composeView).setContent {
                 Text(text = "Hello world.")
             }
