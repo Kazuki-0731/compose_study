724ac8c3728015c3d71a673426c4713532bb2df1
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-06-24 21:35:42 +0900

    add animation test

---
+++ app/src/main/java/com/example/compose_study/ui/animation_test/AnimationViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.compose_study.ui.animation_test
+
+import androidx.lifecycle.ViewModel
+
+class AnimationViewModel : ViewModel() {
+
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is settings Fragment"
+//    }
+//    val text: LiveData<String> = _text
+}
